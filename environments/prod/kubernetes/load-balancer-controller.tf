8588ea818eb2551555725a9261d332a976bafcce
Harshit Saxena <harshit214saxena@gmail.com>
2026-09-13 16:57:10 +0530

    feat: deploy AWS Load Balancer Controller

---
+++ environments/prod/kubernetes/load-balancer-controller.tf
@@ -0,0 +1,41 @@
+resource "helm_release" "lbc" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.2"
+  wait       = true
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  # Helm creates the ServiceAccount and applies the IRSA annotation
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  # IRSA annotation: links the ServiceAccount to the IAM role
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = data.aws_iam_role.lbc.arn
+  }
+
+  set {
+    name  = "region"
+    value = var.aws_region
+  }
+
+  set {
+    name  = "vpcId"
+    value = data.aws_eks_cluster.this.vpc_config[0].vpc_id
+  }
+}
+
