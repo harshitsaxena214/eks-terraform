23607306acdb4195cab818c4a34f04963e63c5ba
Harshit Saxena <harshit214saxena@gmail.com>
2026-09-13 16:24:12 +0530

    feat: provision AWS networking

---
+++ environments/prod/infra/vpc.tf
@@ -0,0 +1,31 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${local.name_prefix}-vpc"
+  cidr = var.vpc_cidr
+
+  azs              = local.azs
+  public_subnets   = var.public_subnet_cidrs
+  private_subnets  = var.private_subnet_cidrs
+  database_subnets = var.database_subnet_cidrs
+
+  create_igw = true
+
+  # PROD: one NAT per AZ Ã¢â‚¬â€ higher cost but highly available
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
---
+++ environments/staging/infra/vpc.tf
@@ -0,0 +1,30 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${local.name_prefix}-vpc"
+  cidr = var.vpc_cidr
+
+  azs              = local.azs
+  public_subnets   = var.public_subnet_cidrs
+  private_subnets  = var.private_subnet_cidrs
+  database_subnets = var.database_subnet_cidrs
+
+  create_igw = true
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
